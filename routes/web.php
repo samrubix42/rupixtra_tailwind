a1181d6409f979d033fb7707b59e9c650a48092e
Saumy Anand <samcool3203@gmail.com>
2026-01-23 13:47:25 +0530

    Add cache clearing route to improve application maintenance

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use App\Livewire\Public\Home\Home;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 
 Route::livewire('/',Home::class)->name('home');
+
+
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    return "Cache Cleared!";
+});
